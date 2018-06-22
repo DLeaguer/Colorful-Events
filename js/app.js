e5a732e1f8682acab52d884bea892bf763d8e599
DLeaguer <immostlikely@hotmail.com>
2018-06-21 19:32:08 -1000

    completed Colorful-Events

--- js/app.js
+++ js/app.js
@@ -1,1 +1,38 @@
+// var getButton = document.getElementById('insideButton');
+// var inside = document.createEventListener(onclick, 'button');
+// getButton.innerHTML = 'click me!';
+// function button() {
+//    var letters = '0123456789ABCDEF';
+//    var color = '#';
+//    for (var i = 0; i < 6; i++) {
+//     color += letters[Math.floor(Math.random() *16)];
+//    }
+//    return color;
+// }
 
+// function getButton() {
+//     inside.css('backgroun-color', button());
+// }
+
+var getButton = document.createElement('button');
+getButton.innerHTML = 'click me!';
+getButton.id = 'button';
+document.body.appendChild(getButton);
+getButton.addEventListener('click', changeColor);
+
+function makeColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function changeColor() {
+    document.body.style.backgroundColor = makeColor();
+}
+
+function changeInside() {
+    
+}
